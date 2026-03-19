edf91f324db9d7cc39659c9fd77cc25ab032a406
krookskala <krookskala@gmail.com>
2026-03-19 14:21:10 +0100

    Add AuthModule to handle authentication logic, integrating Passport and JWT for secure user sessions

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./jwt.strategy";
+import { PassportModule } from "@nestjs/passport";
+import { jwtConstants } from "./constants";
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: "1d" }
+    })
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController]
+})
+export class AuthModule {}
