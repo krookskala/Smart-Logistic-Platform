6b25d59692228ab92053e53179f1cdfdc6e7f569
krookskala <krookskala@gmail.com>
2026-03-19 14:28:36 +0100

    Add CouriersModule to encapsulate courier-related functionality, integrating CouriersController and CouriersService for managing courier operations.

---
+++ backend/src/couriers/couriers.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { CouriersController } from "./couriers.controller";
+import { CouriersService } from "./couriers.service";
+
+@Module({
+  controllers: [CouriersController],
+  providers: [CouriersService]
+})
+export class CouriersModule {}
