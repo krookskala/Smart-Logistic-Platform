2ef2123842d5327e8031de212a56aaaa75e857bc
krookskala <ismailsariarslan7@gmail.com>
2026-03-22 14:46:14 +0100

    Add Swagger Annotations to Users Controller
    - Introduced ApiTags and ApiBearerAuth decorators to the UsersController for improved API documentation and authentication handling.
    - Enhanced the organization and clarity of the API endpoints related to users.

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Request
 } from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { UsersService } from "./users.service";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 import { Roles } from "../auth/roles.decorator";
@@ -14,6 +15,8 @@ import { RolesGuard } from "../auth/roles.guard";
 import { UpdateUserRoleDto } from "./dto/update-user-role.dto";
 import { AuthUser } from "../auth/auth-user.type";
 
+@ApiTags("Users")
+@ApiBearerAuth()
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
